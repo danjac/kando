0b02fc053e573452680f00ba20e1009b9768d921
Dan Jacob <danjac2018@gmail.com>
2020-11-28 02:05:32 +0200

    Project members model

--- kando/projects/rules.py
+++ kando/projects/rules.py
@@ -3,24 +3,25 @@ import rules
 
 
 @rules.predicate
-def is_owner(user, project):
+def is_project_owner(user, project):
     return user == project.owner
 
 
 @rules.predicate
-def is_member(user, project):
+def is_project_member(user, project):
     return user.is_authenticated and user.is_member(project)
 
 
 @rules.predicate
-def is_manager(user, project):
+def is_project_manager(user, project):
     return user.is_authenticated and user.is_manager(project)
 
 
 @rules.predicate
-def is_admin(user, project):
+def is_project_admin(user, project):
     return user.is_authenticated and user.is_admin(project)
 
 
-rules.add_perm("projects.change_project", is_owner | is_admin)
-rules.add_perm("projects.delete_project", is_owner)
+rules.add_perm("projects.view_project", is_project_owner | is_project_member)
+rules.add_perm("projects.change_project", is_project_owner | is_project_admin)
+rules.add_perm("projects.delete_project", is_project_owner)
