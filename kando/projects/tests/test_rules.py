07958d19dfb6cb585f40c43f9ddefd041917e612
Dan Jacob <danjac2018@gmail.com>
2020-11-28 22:22:11 +0200

    Card menu

--- kando/projects/tests/test_rules.py
+++ kando/projects/tests/test_rules.py
@@ -13,32 +13,38 @@ class TestPermissions:
         assert project.owner.has_perm("projects.view_project", project)
         assert project.owner.has_perm("projects.change_project", project)
         assert project.owner.has_perm("projects.delete_project", project)
+        assert project.owner.has_perm("projects.add_members", project)
 
     def test_anonymous_user(self, anonymous_user, project):
         assert not anonymous_user.has_perm("projects.view_project", project)
         assert not anonymous_user.has_perm("projects.change_project", project)
         assert not anonymous_user.has_perm("projects.delete_project", project)
+        assert not anonymous_user.has_perm("projects.add_members", project)
 
     def test_non_member(self, user):
         project = ProjectFactory()
         assert not user.has_perm("projects.view_project", project)
         assert not user.has_perm("projects.change_project", project)
         assert not user.has_perm("projects.delete_project", project)
+        assert not user.has_perm("projects.add_members", project)
 
     def test_member(self, user):
         member = ProjectMemberFactory(user=user, role=ProjectMember.Role.MEMBER)
         assert user.has_perm("projects.view_project", member.project)
         assert not user.has_perm("projects.change_project", member.project)
         assert not user.has_perm("projects.delete_project", member.project)
+        assert not user.has_perm("projects.add_members", member.project)
 
     def test_manager(self, user):
         member = ProjectMemberFactory(user=user, role=ProjectMember.Role.MANAGER)
         assert user.has_perm("projects.view_project", member.project)
         assert not user.has_perm("projects.change_project", member.project)
         assert not user.has_perm("projects.delete_project", member.project)
+        assert not user.has_perm("projects.add_members", member.project)
 
     def test_admin(self, user):
         member = ProjectMemberFactory(user=user, role=ProjectMember.Role.ADMIN)
         assert user.has_perm("projects.view_project", member.project)
         assert user.has_perm("projects.change_project", member.project)
         assert not user.has_perm("projects.delete_project", member.project)
+        assert user.has_perm("projects.add_members", member.project)
