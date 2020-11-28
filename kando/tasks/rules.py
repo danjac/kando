bb90b1a7a6148c9f061b8d40235010f752acaf67
Dan Jacob <danjac2018@gmail.com>
2020-11-29 01:57:28 +0200

    Form plugin

--- kando/tasks/rules.py
+++ kando/tasks/rules.py
@@ -4,12 +4,6 @@ import rules
 # Kando
 from kando.cards.rules import is_card_owner_or_assignee
 
-
-@rules.predicate
-def is_task_owner(user, task):
-    return user.is_authenticated and task.owner == user
-
-
 rules.add_perm("tasks.create_task", is_card_owner_or_assignee)
 rules.add_perm("tasks.update_task", is_card_owner_or_assignee)
 rules.add_perm("tasks.delete_task", is_card_owner_or_assignee)
